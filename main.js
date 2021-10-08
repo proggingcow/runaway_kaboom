f3149e951a2790b6ba46df0138d3c5b7f9d72950
toby <tobiahstoodley@gmail.com>
2021-10-08 12:39:05 +0100

    now has background

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const k = kaboom({global:true});
+const k = kaboom({global:true,width:640,height:480,stretch:false});
 
 loadRoot("assets/")
 loadSprite("cow","cow.png")
@@ -10,6 +10,7 @@ loadSprite("sumoner","sumoner.png")
 
 
 const ss = k.scene("s",() =>{
+  let background = add([sprite("background"),pos(0,0)])
   let player = add([sprite("cow"),origin("center"),area(vec2(-11,-11),vec2(11,11)),pos(320,240)])
 
 });
