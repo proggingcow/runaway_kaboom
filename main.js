b83961fa1a0965cc7c4a679bea2f2067ca692094
toby <tobiahstoodley@gmail.com>
2021-10-12 12:06:08 +0100

    now has moving cow

--- main.js
+++ main.js
@@ -8,11 +8,26 @@ loadSprite("sumoner","sumoner.png")
 
 
 
+function mouseFollow(){
+    return{
+        add(){
+            this.action(()=>{
+                let mp = mouseWorldPos();
+                if (this.pos.x < mp.x ) this.pos.x += 10* dt();
+                if (this.pos.y < mp.y ) this.pos.y += 10* dt();
+                if (this.pos.x > mp.x ) this.pos.x -= 10* dt();
+                if (this.pos.y > mp.y ) this.pos.y -= 10* dt();
+            })
+        }
+    }
+}
+
 
 const ss = k.scene("s",() =>{
   let background = add([sprite("background"),pos(0,0)])
-  let player = add([sprite("cow"),origin("center"),area(vec2(-11,-11),vec2(11,11)),pos(320,240)])
-
+  let player = add([sprite("cow"),pos(320,240),origin("center"),area(vec2(-11,-11),vec2(11,11)),mouseFollow()])
+  let sumoner = add([sprite("sumoner"),scale(1.5,1.5),pos(320,0),origin("center"),z(2)])
 });
 
 k.go("s")
+
